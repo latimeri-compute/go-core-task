118bb55c67f9f8a9020bf75262df7304bb24e708
latimeri <var.trusova@gmail.com>
2025-08-02 17:29:44 +0300

    задание 5 выполнено

---
+++ 5/main5_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestGetIntersection(t *testing.T) {
+	tests := []struct {
+		name      string
+		slice1    []int
+		slice2    []int
+		wantBool  bool
+		wantSlice []int
+	}{
+		{
+			name:      "пересекаются",
+			slice1:    []int{65, 3, 58, 678, 64},
+			slice2:    []int{64, 2, 3, 43},
+			wantSlice: []int{64, 3},
+			wantBool:  true,
+		},
+		{
+			name:      "не пересекаются",
+			slice1:    []int{65, 3, 58, 678, 64},
+			slice2:    []int{2, 43},
+			wantSlice: []int{},
+			wantBool:  false,
+		},
+		{
+			name:      "не пересекаются",
+			slice1:    []int{},
+			slice2:    []int{2, 43},
+			wantSlice: []int{},
+			wantBool:  false,
+		},
+		{
+			name:      "не пересекаются",
+			slice1:    []int{65, 3, 58, 678, 64},
+			slice2:    []int{},
+			wantSlice: []int{},
+			wantBool:  false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, ok := getIntersection(test.slice1, test.slice2)
+
+			if ok != test.wantBool {
+				t.Errorf("got: %v; want: %v", ok, test.wantBool)
+			}
+
+			if !slices.Equal(got, test.wantSlice) {
+				t.Errorf("got: %v; want: %v", got, test.wantSlice)
+			}
+		})
+	}
+}
